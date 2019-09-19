937d680f9f4dc2bd1b88e368bf869c73c6d68a08
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2019-09-20 05:38:03 +0900

    Update build_MIxnyn.py

--- MI_kraskov/build_MIxnyn.py
+++ MI_kraskov/build_MIxnyn.py
@@ -5,7 +5,7 @@ from cffi import FFI
 ffi = FFI()
 
 base = os.path.abspath(os.path.dirname(__file__))
-ffi.set_source(os.path.basename(base) + "._MIxnyn",
+ffi.set_source(os.path.basename(base) + "._MIxnyn", "",
                sources=[os.path.join(base, j) for j in ["MIxnyn.C", "miutils.C"]],
                include_dirs=[base],
                libraries=["c"])
