37ca71ddfd319badd7dc27d780f91822aeeeae7b
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2020-04-25 10:10:07 +0900

    Update build_MIxnyn.py

--- MI_kraskov/build_MIxnyn.py
+++ MI_kraskov/build_MIxnyn.py
@@ -10,8 +10,7 @@ for j in ["MIxnyn.C", "miutils.C"]:
     with open(os.path.join(base, j)) as f:
         source += f.read()
 ffi.set_source(os.path.basename(base) + "._MIxnyn", source,
-               include_dirs=[base],
-               libraries=["c"])
+               include_dirs=[base])
 
 ffi.cdef("""
 double MIxnyn(double *_x, int dimx, int dimy, int N, int K, double addnoise);
